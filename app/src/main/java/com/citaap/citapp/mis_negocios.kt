c712a0052bc173ad7ab36f0079e51ee199c2d595
Rodrigo <rodrigoafz26@gmail.com>
2024-10-23 17:21:32 -0500

    Commit de prueba

--- app/src/main/java/com/citaap/citapp/mis_negocios.kt
+++ app/src/main/java/com/citaap/citapp/mis_negocios.kt
@@ -1,2 +1,16 @@
 package com.citaap.citapp
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.citaap.citapp.ui.theme.CitAppTheme
+
+
+@Composable
+
+fun mis_negocios(){
+    CitAppTheme {
+
+    }
+}
